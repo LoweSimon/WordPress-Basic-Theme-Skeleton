35288530dcf8607562d2b22276154e1f7adfe2ea
Simon <40033923+LoweSimon@users.noreply.github.com>
2024-01-13 15:34:39 +0000

    Uploading files.

---
+++ wp-theme/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<section>
+    <div class="wrapper">
+        <article class="content px-3 py-5 p-md-5">
+	    
+            <h1>Page Not Found</h1>
+
+            <?php
+                get_search_form();
+            ?>
+			
+	    </article>
+
+    </div>
+</section>
+<?php get_footer(); ?>
---
+++ wp-theme/footer.php
@@ -0,0 +1,12 @@
+	<footer class="footer text-center py-2 theme-bg-dark">
+		   
+	   
+	 
+	</footer>
+	
+	<?php 
+		wp_footer();
+	?>
+
+</body>
+</html> 
